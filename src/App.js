69a8fbd4815b74c2df865bbaa517661e056f3413
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2022-11-18 02:15:51 -0500

    add title and tables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon search</h1>
+
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Type</th>
+        </thead>
+        <tbody>
+          <td>Bulbasaur</td>
+          <td>Grass, Poison</td>
+        </tbody>
+      </table>
     </div>
   );
 }
